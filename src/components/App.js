51c7ab693c0703b58350b1ff26f00d75eb68b8c8
ayushsahu <ayushsahu97810@gmail.com>
2025-08-04 11:53:44 +0530

    abcd

--- src/components/App.js
+++ src/components/App.js
@@ -1,3 +1,17 @@
-<p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+import react from 'react';
 
-h1
+function App(){
+    return (
+      <div id="main">
+      <p>
+        Now I can render any React component on any DOM node I want using ReactDOM.render
+      </p>
+    </div>
+    )
+}
+
+export default App;
+
+
+
+
